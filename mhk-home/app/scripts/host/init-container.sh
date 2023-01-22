ed9fc2afe54691b464563c98040aab70617c7ae2
Joaquim Carvalho <joaquim@uc.pt>
2023-01-22 12:20:40 +0000

    Fix postCreateCommand

--- mhk-home/app/scripts/host/init-container.sh
+++ mhk-home/app/scripts/host/init-container.sh
@@ -7,11 +7,8 @@
 
 # Clean CRLF see https://stackoverflow.com/questions/48692741/how-can-i-make-all-line-endings-eols-in-all-files-in-visual-studio-code-unix
 git config core.autocrlf false
-
 git rm --cached -r .         # Don’t forget the dot at the end
-
 git reset --hard
-
 cd mhk-home
 
 . ./app/scripts/host/manager-init.sh
