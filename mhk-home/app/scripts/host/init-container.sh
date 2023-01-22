e68b9916fad17fc8718e218fe531a7392e92c002
Joaquim Carvalho <joaquim@uc.pt>
2023-01-22 14:31:27 +0000

    Move git commands to postCreateCommand

--- mhk-home/app/scripts/host/init-container.sh
+++ mhk-home/app/scripts/host/init-container.sh
@@ -1,14 +1,8 @@
 # Script to finish setup to devcontainer
-# It cleans CRLF added by git clone on windows
-# And calls manager-init.sh
-# 
-# Note that the script changes files in the user 
-# home directory
 
 # Clean CRLF see https://stackoverflow.com/questions/48692741/how-can-i-make-all-line-endings-eols-in-all-files-in-visual-studio-code-unix
-git config core.autocrlf false
-git rm --cached -r .         # Don’t forget the dot at the end
-git reset --hard
+
 cd mhk-home
 
 . ./app/scripts/host/manager-init.sh
+echo "Open a new terminal and do 'mhk --help'"
